25e2eeb5ff738c9e8bf0feeb346e4bac16c9307f
nathn101 <nathan.ch.chu@gmail.com>
2023-05-13 17:12:53 -0400

    Updated contract and slippage calculation

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { ethers, BigNumber } from 'ethers';
-import { ETH, USDC } from './tokens';
+import { ETH, USDC, WETH} from './tokens';
+import { CoinGeckoClient, SimplePriceResponse } from 'coingecko-api-v3';
+import fs from 'fs';
 
 const FROM_TOKEN = USDC;
 const FROM_BALANCE = BigNumber.from('1000000');
@@ -9,43 +11,79 @@ const TO_TOKEN = ETH;
   console.info(`Converting ${FROM_BALANCE.toString()} ${FROM_TOKEN.symbol} to ${TO_TOKEN.symbol}`);
 
   // Get the contract for a DEX.
-  // Uniswap contract address: https://etherscan.io/token/0x1f9840a85d5af5bf1d1762f925bdaddc4201f984#code
 
-  const uniAddress = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
+  // Uniswap Router contract address: https://etherscan.io/address/0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D#code
+  const uniAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
-  const uniAbi = [{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"address","name":"minter_","type":"address"},{"internalType":"uint256","name":"mintingAllowedAfter_","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"delegator","type":"address"},{"indexed":true,"internalType":"address","name":"fromDelegate","type":"address"},{"indexed":true,"internalType":"address","name":"toDelegate","type":"address"}],"name":"DelegateChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"delegate","type":"address"},{"indexed":false,"internalType":"uint256","name":"previousBalance","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"newBalance","type":"uint256"}],"name":"DelegateVotesChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"minter","type":"address"},{"indexed":false,"internalType":"address","name":"newMinter","type":"address"}],"name":"MinterChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"DELEGATION_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"DOMAIN_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"rawAmount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint32","name":"","type":"uint32"}],"name":"checkpoints","outputs":[{"internalType":"uint32","name":"fromBlock","type":"uint32"},{"internalType":"uint96","name":"votes","type":"uint96"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"delegatee","type":"address"}],"name":"delegate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"delegatee","type":"address"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"uint256","name":"expiry","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"delegateBySig","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"delegates","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"getCurrentVotes","outputs":[{"internalType":"uint96","name":"","type":"uint96"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"blockNumber","type":"uint256"}],"name":"getPriorVotes","outputs":[{"internalType":"uint96","name":"","type":"uint96"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"minimumTimeBetweenMints","outputs":[{"internalType":"uint32","name":"","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"rawAmount","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"mintCap","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"minter","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"mintingAllowedAfter","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"numCheckpoints","outputs":[{"internalType":"uint32","name":"","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"rawAmount","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"minter_","type":"address"}],"name":"setMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"rawAmount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"src","type":"address"},{"internalType":"address","name":"dst","type":"address"},{"internalType":"uint256","name":"rawAmount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}];
+  // Uniswap Router Application Binary Interface
+  const uniAbi = fs.readFileSync("./router.json").toString();
 
+  // Using the default provider
   const provider = ethers.getDefaultProvider('homestead');
 
-  const uniContract = new ethers.Contract(uniAddress, uniAbi);
-  
-  // uniContract.connect(provider);
+  // Create a contract object from a contract address and ABI
+  let uniContract = new ethers.Contract(uniAddress, uniAbi, provider);  
   
   // Use ethers and the DEX contract to figure out how much TO_TOKEN you can get
   // for the FROM_TOKEN.
-
-  let result: number = uniContract.methods.swapExactTokensForTokens(FROM_TOKEN, TO_TOKEN, FROM_BALANCE);
   
-  // TODO:
-  const swapBalance = BigNumber.from((result as unknown) as string);
+  let result = await uniContract.getAmountsOut(FROM_BALANCE, [FROM_TOKEN.address, WETH.address]);
 
+  let ETHamnt = result[1];
+  // console.log(ETHamnt);
+  // TODO:
+  const swapBalance = BigNumber.from((ETHamnt as unknown) as string);
+
+  // The swapBalance should be in units of wei
   console.info(`Estimated swap balance: ${swapBalance.toString()} ${TO_TOKEN.symbol}`);
 
   // Figure out spot values of tokens.
-  let from_price: number = uniContract.methods.spotPrice(FROM_TOKEN.address);
-  let to_price: number = uniContract.methods.spotPrice(TO_TOKEN.address);
+
+  let client = new CoinGeckoClient({
+    timeout: 10000,
+    autoRetry: true,
+  });
+  
+  async function getTokenPrice(tokenId:string) {
+    try {
+      let response = await client.simplePrice({
+        ids: tokenId,
+        vs_currencies: 'usd'
+      });
+      // console.log(response);
+      return (response[tokenId]['usd']);
+    } catch (error) {
+      console.error('Error retrieving token price:', error);
+      throw error;
+    }
+  }
+
+  let from_price = await getTokenPrice(FROM_TOKEN.coingecko);
+  // let from_price_BN = BigNumber.from((from_price as unknown) as string);
+  // console.log(`From price: ${from_price}`);
+  let to_price_BN = await getTokenPrice(TO_TOKEN.coingecko);
+  let to_price = ethers.utils.parseUnits(to_price_BN.toString(), 2);
+  // console.log(`To price: ${to_price.toBigInt()}`); 
 
   // Calculate slippage on the swap.
   // Money gained from the sale of the USDC tokens
-  const cur = FROM_BALANCE.mul(from_price);
-  // ETH tokens gained from purchasing using the money gained from the sale of the USDC tokens
-  const ETHtokens = cur.div(to_price);
+  const cur = ethers.utils.parseUnits(FROM_BALANCE.toString(), 2);
+  // console.log(cur.toBigInt());
+
+  let wei = cur.mul(ethers.constants.WeiPerEther).div(to_price);
+  // console.log(wei.toBigInt());
+  // console.log(swapBalance.toBigInt());
+
   // Slippage is the difference between the ETH tokens gained from the swap and the ETH tokens gained from a manual transaction
-  const slippage = ETHtokens.sub(swapBalance);
-
-
+  const slippage = BigNumber.from((wei.sub(swapBalance.mul(1000000))).toString());
+  // console.log(slippage.toBigInt());
+  let slip_ether = ethers.utils.formatUnits(slippage, "ether");
+  // console.log(slip_ether);
+  let swap_ether = ethers.utils.formatUnits(swapBalance.mul(1000000), "ether");
+  // console.log(swap_ether);
+  
   // TODO:
-  const slippagePercent = slippage.div(swapBalance);
+  const slippagePercent =  ((slip_ether as unknown) as number) / ((swap_ether as unknown) as number);
 
-  console.info(`Slippage: ${slippagePercent.toNumber() * 100}%`);
+  console.info(`Slippage: ${slippagePercent * 100}%`);
 })();
